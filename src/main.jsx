aba0810ea0e61c55f190a8b16e32355ddc49f5b5
MiltonCoronado <miltonext@hotmail.com>
2026-03-24 01:45:50 -0500

    refactor: remove unused React import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import { App } from './App.jsx';
 import { rootReducer } from './reducer/rootReducer.jsx';
 import { Provider } from 'react-redux';
-import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
+import {
+  applyMiddleware,
+  compose,
+  legacy_createStore as createStore,
+} from 'redux';
 import { logger } from '../src/middlewares/middlewares.jsx';
 import { thunk } from 'redux-thunk';
 import './index.css';
@@ -15,9 +18,8 @@ const composedEnhancer = composeAlt(applyMiddleware(thunk, logger));
 const storeReducerSwitch = createStore(rootReducer, composedEnhancer);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={storeReducerSwitch}>{/* El Provider con la prop store proviene de la biblioteca react-redux. Provider es un componente de alto nivel que envuelve tu aplicación para que cualquier componente pueda acceder al store de Redux(que contiene al reducer en si). a través del contexto de React. */}
-      <App />
-    </Provider>,
-)
-
-
+  <Provider store={storeReducerSwitch}>
+    {/* El Provider con la prop store proviene de la biblioteca react-redux. Provider es un componente de alto nivel que envuelve tu aplicación para que cualquier componente pueda acceder al store de Redux(que contiene al reducer en si). a través del contexto de React. */}
+    <App />
+  </Provider>,
+);

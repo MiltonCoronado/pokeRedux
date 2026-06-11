96673d98dd99fdb404d97cef9fc68277db6afa5e
MiltonCoronado <miltonext@hotmail.com>
2026-06-10 22:13:28 -0500

    feat: Fn. handleChange was added at onChange event at the input

--- src/components/Searcher.jsx
+++ src/components/Searcher.jsx
@@ -1,7 +1,23 @@
 import { Input } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSearch } from '../actions/creators';
 
 const Searcher = () => {
-  return <Input.Search placeholder='Buscar' style={{ marginBottom: 20 }}/>
+  const dispatch = useDispatch();
+  const search = useSelector((state) => state.data.search);
+
+  const handleChange = (event) => {
+    dispatch(setSearch(event.target.value));
+  };
+
+  return (
+    <Input.Search
+      placeholder="Buscar"
+      style={{ marginBottom: 20 }}
+      value={search}
+      onChange={handleChange}
+    />
+  );
 };
 
 export { Searcher };
